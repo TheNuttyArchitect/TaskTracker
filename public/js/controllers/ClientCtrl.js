fde8b24555ae7330988ab4b0b43390e043a6fcc2
TheNuttyArchitect <ben@techtriumphs.com>
2015-05-04 18:34:54 -0400

    Beginning to format the grid

--- public/js/controllers/ClientCtrl.js
+++ public/js/controllers/ClientCtrl.js
@@ -1,7 +1,25 @@
 angular.module('ClientCtrl', [])
     .controller('ClientController', ['$scope','ClientService',function($scope,ClientService){
+        /*$scope.gridOptions.columnDefs = [
+            {field: 'name', visible: true},
+            {field: 'id', visible: false},
+            {field: 'isActive', visible: true},
+            {field: 'createdDate', visible: true},
+            {field: 'modifiedDate', visible: true}
+        ];*/
+
         ClientService.getAll().success(function(data){
-            $scope.clients = data;
+
+            $scope.clientGrid = {
+                    columnDefs: [
+                        {field: 'name', visible: true},
+                        {field: 'id', visible: false},
+                        {field: 'isActive', visible: true},
+                        {field: 'createdDate', visible: true},
+                        {field: 'modifiedDate', visible: true}
+                    ],
+                    data: data
+                };
         });
             //[{FirstName: 'Fred', LastName: 'Flintstone'}];
     }]);
