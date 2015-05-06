595fb8daed1f70c2a25d35895065fccb258b2bb6
TheNuttyArchitect <ben@techtriumphs.com>
2015-05-05 20:15:10 -0400

    save message

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,9 @@
-/*module.exports = function(app){
-  app.get('*', function(req,res){
-    res.sendfile('./public/views/index.html')
-  });
-}*/
-
-//var client = require('../model/client');
-/*var express = require('express');
+var client = require('../model/client');
+var express = require('express');
 var router = express.Router();
 
 router.get('*', function(req,res){
     res.sendFile('./public/index.html');
-
 });
 
-module.exports = router;*/
+module.exports = router;
